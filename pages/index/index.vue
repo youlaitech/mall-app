e07e81d054c0ce7c3982a29208a7a9bbc061475e
haoxr <1490493387@qq.com>
2020-12-15 00:00:02 +0800

    fix:广告加载失败处理

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -6,7 +6,7 @@
 			<input class="ser-input" type="text" value="输入关键字搜索" disabled />
 		</view>
 		<!-- #endif -->
-		
+
 		<!-- 头部轮播 -->
 		<view class="carousel-section">
 			<!-- 标题栏和状态栏占位符 -->
@@ -46,11 +46,11 @@
 				<text>速食生鲜</text>
 			</view>
 		</view>
-		
+
 		<view class="ad-1">
 			<image src="/static/temp/ad1.jpg" mode="scaleToFill"></image>
 		</view>
-		
+
 		<!-- 秒杀楼层 -->
 		<view class="seckill-section m-t">
 			<view class="s-header">
@@ -63,11 +63,7 @@
 			</view>
 			<scroll-view class="floor-list" scroll-x>
 				<view class="scoll-wrapper">
-					<view 
-						v-for="(item, index) in goodsList" :key="index"
-						class="floor-item"
-						@click="navToDetailPage(item)"
-					>
+					<view v-for="(item, index) in goodsList" :key="index" class="floor-item" @click="navToDetailPage(item)">
 						<image :src="item.image" mode="aspectFill"></image>
 						<text class="title clamp">{{item.title}}</text>
 						<text class="price">￥{{item.price}}</text>
@@ -75,7 +71,7 @@
 				</view>
 			</scroll-view>
 		</view>
-		
+
 		<!-- 团购楼层 -->
 		<view class="f-header m-t">
 			<image src="/static/temp/h1.png"></image>
@@ -87,42 +83,37 @@
 		</view>
 		<view class="group-section">
 			<swiper class="g-swiper" :duration="500">
-				<swiper-item
-					class="g-swiper-item"
-					v-for="(item, index) in goodsList" :key="index"
-					v-if="index%2 === 0"
-					@click="navToDetailPage(item)"
-				>
+				<swiper-item class="g-swiper-item" v-for="(item, index) in goodsList" :key="index" v-if="index%2 === 0" @click="navToDetailPage(item)">
 					<view class="g-item left">
 						<image :src="item.image" mode="aspectFill"></image>
 						<view class="t-box">
 							<text class="title clamp">{{item.title}}</text>
 							<view class="price-box">
-								<text class="price">￥{{item.price}}</text> 
-								<text class="m-price">￥188</text> 
+								<text class="price">￥{{item.price}}</text>
+								<text class="m-price">￥188</text>
 							</view>
-							
+
 							<view class="pro-box">
-							  	<view class="progress-box">
-							  		<progress percent="72" activeColor="#fa436a" active stroke-width="6" />
-							  	</view>
+								<view class="progress-box">
+									<progress percent="72" activeColor="#fa436a" active stroke-width="6" />
+								</view>
 								<text>6人成团</text>
 							</view>
 						</view>
-						            
+
 					</view>
 					<view class="g-item right">
 						<image :src="goodsList[index+1].image" mode="aspectFill"></image>
 						<view class="t-box">
 							<text class="title clamp">{{goodsList[index+1].title}}</text>
 							<view class="price-box">
-								<text class="price">￥{{goodsList[index+1].price}}</text> 
-								<text class="m-price">￥188</text> 
+								<text class="price">￥{{goodsList[index+1].price}}</text>
+								<text class="m-price">￥188</text>
 							</view>
 							<view class="pro-box">
-							  	<view class="progress-box">
-							  		<progress percent="72" activeColor="#fa436a" active stroke-width="6" />
-							  	</view>
+								<view class="progress-box">
+									<progress percent="72" activeColor="#fa436a" active stroke-width="6" />
+								</view>
 								<text>10人成团</text>
 							</view>
 						</view>
@@ -131,9 +122,9 @@
 
 			</swiper>
 		</view>
-		
-		
-		
+
+
+
 		<!-- 分类推荐楼层 -->
 		<view class="f-header m-t">
 			<image src="/static/temp/h1.png"></image>
@@ -145,15 +136,12 @@
 		</view>
 		<view class="hot-floor">
 			<view class="floor-img-box">
-				<image class="floor-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553409398864&di=4a12763adccf229133fb85193b7cc08f&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201703%2F19%2F20170319150032_MNwmn.jpeg" mode="scaleToFill"></image>
+				<image class="floor-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553409398864&di=4a12763adccf229133fb85193b7cc08f&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201703%2F19%2F20170319150032_MNwmn.jpeg"
+				 mode="scaleToFill"></image>
 			</view>
 			<scroll-view class="floor-list" scroll-x>
 				<view class="scoll-wrapper">
-					<view 
-						v-for="(item, index) in goodsList" :key="index"
-						class="floor-item"
-						@click="navToDetailPage(item)"
-					>
+					<view v-for="(item, index) in goodsList" :key="index" class="floor-item" @click="navToDetailPage(item)">
 						<image :src="item.image" mode="aspectFill"></image>
 						<text class="title clamp">{{item.title}}</text>
 						<text class="price">￥{{item.price}}</text>
@@ -167,15 +155,12 @@
 		</view>
 		<view class="hot-floor">
 			<view class="floor-img-box">
-				<image class="floor-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553409984228&di=dee176242038c2d545b7690b303d65ea&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F5ef4da9f17faaf4612f0d5046f4161e556e9bbcfdb5b-rHjf00_fw658" mode="scaleToFill"></image>
+				<image class="floor-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553409984228&di=dee176242038c2d545b7690b303d65ea&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F5ef4da9f17faaf4612f0d5046f4161e556e9bbcfdb5b-rHjf00_fw658"
+				 mode="scaleToFill"></image>
 			</view>
 			<scroll-view class="floor-list" scroll-x>
 				<view class="scoll-wrapper">
-					<view 
-						v-for="(item, index) in goodsList" :key="index"
-						class="floor-item"
-						@click="navToDetailPage(item)"
-					>
+					<view v-for="(item, index) in goodsList" :key="index" class="floor-item" @click="navToDetailPage(item)">
 						<image :src="item.image3" mode="aspectFill"></image>
 						<text class="title clamp">{{item.title}}</text>
 						<text class="price">￥{{item.price}}</text>
@@ -189,15 +174,12 @@
 		</view>
 		<view class="hot-floor">
 			<view class="floor-img-box">
-				<image class="floor-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553409794730&di=12b840ec4f5748ef06880b85ff63e34e&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01dc03589ed568a8012060c82ac03c.jpg%40900w_1l_2o_100sh.jpg" mode="scaleToFill"></image>
+				<image class="floor-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553409794730&di=12b840ec4f5748ef06880b85ff63e34e&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01dc03589ed568a8012060c82ac03c.jpg%40900w_1l_2o_100sh.jpg"
+				 mode="scaleToFill"></image>
 			</view>
 			<scroll-view class="floor-list" scroll-x>
 				<view class="scoll-wrapper">
-					<view 
-						v-for="(item, index) in goodsList" :key="index"
-						class="floor-item"
-						@click="navToDetailPage(item)"
-					>
+					<view v-for="(item, index) in goodsList" :key="index" class="floor-item" @click="navToDetailPage(item)">
 						<image :src="item.image2" mode="aspectFill"></image>
 						<text class="title clamp">{{item.title}}</text>
 						<text class="price">￥{{item.price}}</text>
@@ -219,13 +201,9 @@
 			</view>
 			<text class="yticon icon-you"></text>
 		</view>
-		
+
 		<view class="guess-section">
-			<view 
-				v-for="(item, index) in goodsList" :key="index"
-				class="guess-item"
-				@click="navToDetailPage(item)"
-			>
+			<view v-for="(item, index) in goodsList" :key="index" class="guess-item" @click="navToDetailPage(item)">
 				<view class="image-wrapper">
 					<image :src="item.image" mode="aspectFill"></image>
 				</view>
@@ -233,14 +211,15 @@
 				<text class="price">￥{{item.price}}</text>
 			</view>
 		</view>
-		
+
 
 	</view>
 </template>
 
 <script>
-	
-	import {getAdvertList} from '@/api/sms/advert.js'
+	import {
+		getAdvertList
+	} from '@/api/sms/advert.js'
 
 	export default {
 
@@ -262,11 +241,16 @@
 			 * 分次请求未作整合
 			 */
 			async loadData() {
-				 // let carouselList = await this.$api.json('carouselList');
-				const carouselList=(await getAdvertList()).data
-				this.swiperLength = carouselList.length;
-				this.carouselList = carouselList;
-				
+				const that = this
+				getAdvertList().then(response => {
+					const data = response.data
+					if (data) {
+						that.swiperLength = data.length
+						that.carouselList = data
+					}
+				})
+
+
 				let goodsList = await this.$api.json('goodsList');
 				this.goodsList = goodsList || [];
 			},
@@ -314,57 +298,66 @@
 
 <style lang="scss">
 	/* #ifdef MP */
-	.mp-search-box{
-		position:absolute;
+	.mp-search-box {
+		position: absolute;
 		left: 0;
 		top: 30upx;
 		z-index: 9999;
 		width: 100%;
 		padding: 0 80upx;
-		.ser-input{
-			flex:1;
+
+		.ser-input {
+			flex: 1;
 			height: 56upx;
 			line-height: 56upx;
 			text-align: center;
 			font-size: 28upx;
-			color:$font-color-base;
+			color: $font-color-base;
 			border-radius: 20px;
-			background: rgba(255,255,255,.6);
+			background: rgba(255, 255, 255, .6);
 		}
 	}
-	page{
-		.cate-section{
-			position:relative;
-			z-index:5;
-			border-radius:16upx 16upx 0 0;
-			margin-top:-20upx;
+
+	page {
+		.cate-section {
+			position: relative;
+			z-index: 5;
+			border-radius: 16upx 16upx 0 0;
+			margin-top: -20upx;
 		}
-		.carousel-section{
+
+		.carousel-section {
 			padding: 0;
+
 			.titleNview-placing {
 				padding-top: 0;
 				height: 0;
 			}
-			.carousel{
-				.carousel-item{
+
+			.carousel {
+				.carousel-item {
 					padding: 0;
 				}
 			}
-			.swiper-dots{
-				left:45upx;
-				bottom:40upx;
+
+			.swiper-dots {
+				left: 45upx;
+				bottom: 40upx;
 			}
 		}
 	}
+
 	/* #endif */
-	
-	
+
+
 	page {
 		background: #f5f5f5;
 	}
-	.m-t{
+
+	.m-t {
 		margin-top: 16upx;
 	}
+
 	/* 头部 轮播图 */
 	.carousel-section {
 		position: relative;
@@ -385,6 +378,7 @@
 			transition: .4s;
 		}
 	}
+
 	.carousel {
 		width: 100%;
 		height: 350upx;
@@ -402,6 +396,7 @@
 			border-radius: 10upx;
 		}
 	}
+
 	.swiper-dots {
 		display: flex;
 		position: absolute;
@@ -432,14 +427,16 @@
 			transform: translateX(-50%);
 		}
 	}
+
 	/* 分类 */
 	.cate-section {
 		display: flex;
 		justify-content: space-around;
 		align-items: center;
-		flex-wrap:wrap;
-		padding: 30upx 22upx; 
+		flex-wrap: wrap;
+		padding: 30upx 22upx;
 		background: #fff;
+
 		.cate-item {
 			display: flex;
 			flex-direction: column;
@@ -447,6 +444,7 @@
 			font-size: $font-sm + 2upx;
 			color: $font-color-dark;
 		}
+
 		/* 原图标颜色太深,不想改图了,所以加了透明度 */
 		image {
 			width: 88upx;
@@ -457,230 +455,272 @@
 			box-shadow: 4upx 4upx 20upx rgba(250, 67, 106, 0.3);
 		}
 	}
-	.ad-1{
+
+	.ad-1 {
 		width: 100%;
 		height: 210upx;
 		padding: 10upx 0;
 		background: #fff;
-		image{
-			width:100%;
-			height: 100%; 
+
+		image {
+			width: 100%;
+			height: 100%;
 		}
 	}
+
 	/* 秒杀专区 */
-	.seckill-section{
+	.seckill-section {
 		padding: 4upx 30upx 24upx;
 		background: #fff;
-		.s-header{
-			display:flex;
-			align-items:center;
+
+		.s-header {
+			display: flex;
+			align-items: center;
 			height: 92upx;
 			line-height: 1;
-			.s-img{
+
+			.s-img {
 				width: 140upx;
 				height: 30upx;
 			}
-			.tip{
+
+			.tip {
 				font-size: $font-base;
 				color: $font-color-light;
 				margin: 0 20upx 0 40upx;
 			}
-			.timer{
-				display:inline-block;
+
+			.timer {
+				display: inline-block;
 				width: 40upx;
 				height: 36upx;
-				text-align:center;
+				text-align: center;
 				line-height: 36upx;
 				margin-right: 14upx;
 				font-size: $font-sm+2upx;
 				color: #fff;
 				border-radius: 2px;
-				background: rgba(0,0,0,.8);
+				background: rgba(0, 0, 0, .8);
 			}
-			.icon-you{
+
+			.icon-you {
 				font-size: $font-lg;
 				color: $font-color-light;
 				flex: 1;
 				text-align: right;
 			}
 		}
-		.floor-list{
+
+		.floor-list {
 			white-space: nowrap;
 		}
-		.scoll-wrapper{
-			display:flex;
+
+		.scoll-wrapper {
+			display: flex;
 			align-items: flex-start;
 		}
-		.floor-item{
+
+		.floor-item {
 			width: 150upx;
 			margin-right: 20upx;
 			font-size: $font-sm+2upx;
 			color: $font-color-dark;
 			line-height: 1.8;
-			image{
+
+			image {
 				width: 150upx;
 				height: 150upx;
 				border-radius: 6upx;
 			}
-			.price{
+
+			.price {
 				color: $uni-color-primary;
 			}
 		}
 	}
-	
-	.f-header{
-		display:flex;
-		align-items:center;
+
+	.f-header {
+		display: flex;
+		align-items: center;
 		height: 140upx;
 		padding: 6upx 30upx 8upx;
 		background: #fff;
-		image{
+
+		image {
 			flex-shrink: 0;
 			width: 80upx;
 			height: 80upx;
 			margin-right: 20upx;
 		}
-		.tit-box{
+
+		.tit-box {
 			flex: 1;
 			display: flex;
 			flex-direction: column;
 		}
-		.tit{
+
+		.tit {
 			font-size: $font-lg +2upx;
 			color: #font-color-dark;
 			line-height: 1.3;
 		}
-		.tit2{
+
+		.tit2 {
 			font-size: $font-sm;
 			color: $font-color-light;
 		}
-		.icon-you{
+
+		.icon-you {
 			font-size: $font-lg +2upx;
 			color: $font-color-light;
 		}
 	}
+
 	/* 团购楼层 */
-	.group-section{
+	.group-section {
 		background: #fff;
-		.g-swiper{
+
+		.g-swiper {
 			height: 650upx;
 			padding-bottom: 30upx;
 		}
-		.g-swiper-item{
+
+		.g-swiper-item {
 			width: 100%;
 			padding: 0 30upx;
-			display:flex;
+			display: flex;
 		}
-		image{
+
+		image {
 			width: 100%;
 			height: 460upx;
 			border-radius: 4px;
 		}
-		.g-item{
-			display:flex;
+
+		.g-item {
+			display: flex;
 			flex-direction: column;
-			overflow:hidden;
+			overflow: hidden;
 		}
-		.left{
+
+		.left {
 			flex: 1.2;
 			margin-right: 24upx;
-			.t-box{
+
+			.t-box {
 				padding-top: 20upx;
 			}
 		}
-		.right{
+
+		.right {
 			flex: 0.8;
 			flex-direction: column-reverse;
-			.t-box{
+
+			.t-box {
 				padding-bottom: 20upx;
 			}
 		}
-		.t-box{
+
+		.t-box {
 			height: 160upx;
 			font-size: $font-base+2upx;
 			color: $font-color-dark;
 			line-height: 1.6;
 		}
-		.price{
-			color:$uni-color-primary;
+
+		.price {
+			color: $uni-color-primary;
 		}
-		.m-price{
+
+		.m-price {
 			font-size: $font-sm+2upx;
 			text-decoration: line-through;
 			color: $font-color-light;
 			margin-left: 8upx;
 		}
-		.pro-box{
-			display:flex;
-			align-items:center;
+
+		.pro-box {
+			display: flex;
+			align-items: center;
 			margin-top: 10upx;
 			font-size: $font-sm;
 			color: $font-base;
 			padding-right: 10upx;
 		}
-		.progress-box{
+
+		.progress-box {
 			flex: 1;
 			border-radius: 10px;
 			overflow: hidden;
 			margin-right: 8upx;
 		}
 	}
+
 	/* 分类推荐楼层 */
-	.hot-floor{
+	.hot-floor {
 		width: 100%;
 		overflow: hidden;
 		margin-bottom: 20upx;
-		.floor-img-box{
+
+		.floor-img-box {
 			width: 100%;
-			height:320upx;
-			position:relative;
-			&:after{
+			height: 320upx;
+			position: relative;
+
+			&:after {
 				content: '';
-				position:absolute;
+				position: absolute;
 				left: 0;
 				top: 0;
 				width: 100%;
 				height: 100%;
-				background: linear-gradient(rgba(255,255,255,.06) 30%, #f8f8f8);
+				background: linear-gradient(rgba(255, 255, 255, .06) 30%, #f8f8f8);
 			}
 		}
-		.floor-img{
+
+		.floor-img {
 			width: 100%;
 			height: 100%;
 		}
-		.floor-list{
+
+		.floor-list {
 			white-space: nowrap;
 			padding: 20upx;
 			padding-right: 50upx;
 			border-radius: 6upx;
-			margin-top:-140upx;
+			margin-top: -140upx;
 			margin-left: 30upx;
 			background: #fff;
-			box-shadow: 1px 1px 5px rgba(0,0,0,.2);
+			box-shadow: 1px 1px 5px rgba(0, 0, 0, .2);
 			position: relative;
 			z-index: 1;
 		}
-		.scoll-wrapper{
-			display:flex;
+
+		.scoll-wrapper {
+			display: flex;
 			align-items: flex-start;
 		}
-		.floor-item{
+
+		.floor-item {
 			width: 180upx;
 			margin-right: 20upx;
 			font-size: $font-sm+2upx;
 			color: $font-color-dark;
 			line-height: 1.8;
-			image{
+
+			image {
 				width: 180upx;
 				height: 180upx;
 				border-radius: 6upx;
 			}
-			.price{
+
+			.price {
 				color: $uni-color-primary;
 			}
 		}
-		.more{
-			display:flex;
+
+		.more {
+			display: flex;
 			align-items: center;
 			justify-content: center;
 			flex-direction: column;
@@ -691,48 +731,54 @@
 			background: #f3f3f3;
 			font-size: $font-base;
 			color: $font-color-light;
-			text:first-child{
+
+			text:first-child {
 				margin-bottom: 4upx;
 			}
 		}
 	}
+
 	/* 猜你喜欢 */
-	.guess-section{
-		display:flex;
-		flex-wrap:wrap;
+	.guess-section {
+		display: flex;
+		flex-wrap: wrap;
 		padding: 0 30upx;
 		background: #fff;
-		.guess-item{
-			display:flex;
+
+		.guess-item {
+			display: flex;
 			flex-direction: column;
 			width: 48%;
 			padding-bottom: 40upx;
-			&:nth-child(2n+1){
+
+			&:nth-child(2n+1) {
 				margin-right: 4%;
 			}
 		}
-		.image-wrapper{
+
+		.image-wrapper {
 			width: 100%;
 			height: 330upx;
 			border-radius: 3px;
 			overflow: hidden;
-			image{
+
+			image {
 				width: 100%;
 				height: 100%;
 				opacity: 1;
 			}
 		}
-		.title{
+
+		.title {
 			font-size: $font-lg;
 			color: $font-color-dark;
 			line-height: 80upx;
 		}
-		.price{
+
+		.price {
 			font-size: $font-lg;
 			color: $uni-color-primary;
 			line-height: 1;
 		}
 	}
-	
-
 </style>
